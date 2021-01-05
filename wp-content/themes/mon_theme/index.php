bade46c0169623dfbcd708f332068c14da7b985e
Florent <florent.bailly62400@gmail.com>
2021-01-05 16:30:15 +0100

    add style and text

--- wp-content/themes/mon_theme/index.php
+++ wp-content/themes/mon_theme/index.php
@@ -4,9 +4,29 @@
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Document</title>
+
+    <link rel="stylesheet" type="text/css" href="<?php echo get_stylesheet_directory_uri(). '/style.css' ?>">
+
 </head>
 <body>
     <?php get_header(); ?>
+
+        <div class="container">
+            <div class="text-container">
+                <h2>L'éléction du plus grand patissiers de France</h2>
+                
+                <p class="edition">1ère édition</p>
+                <p class="justify">Lorem ipsum dolor, sit amet consectetur adipisicing elit. Magni veritatis expedita quasi ad quos laboriosam cum veniam voluptatem, nesciunt dolorem.
+                    A, quo nulla placeat error, perspiciatis beatae nostrum voluptates inventore, est nisi reiciendis! Porro modi quidem,
+                    dolores debitis harum molestias esse animi excepturi laboriosam labore, accusantium eligendi nostrum dignissimos natus?
+                    Lorem ipsum dolor sit amet consectetur adipisicing elit. Nostrum corrupti autem odio reiciendis voluptatibus cupiditate expedita in reprehenderit.
+                    Fugit, eaque, provident in est earum voluptas suscipit nostrum saepe perspiciatis, dolores accusamus sint! Natus dicta perspiciatis magnam quibusdam
+                    dolorem quia accusantium distinctio voluptate! Reprehenderit, labore odit dolores ea distinctio ex similique.
+                </p>
+                
+            </div>
+        </div>
+
 
 </body>
 </html>
