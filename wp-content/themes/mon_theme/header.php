c411f1b130fd27de02b7ca24f63dcbfd656ad489
Florent <florent.bailly62400@gmail.com>
2021-01-05 12:30:33 +0100

    add normalize

--- wp-content/themes/mon_theme/header.php
+++ wp-content/themes/mon_theme/header.php
@@ -5,12 +5,17 @@
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Document</title>
     <link rel="stylesheet" type="text/css" href="<?php echo get_stylesheet_directory_uri(). '/css/header.css' ?>">
+    <link rel="stylesheet" type="text/css" href="<?php echo get_stylesheet_directory_uri(). '/style.css' ?>">
 
     <?php wp_head(); ?>
 </head>
 <body>
 
-    <h1>test</h1>
+    <header>
+        <div class="header-center">
+            <h1>ELECT</h1>
+        </div>
+    </header>
     
 </body>
 </html>
