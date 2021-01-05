44c17a9ae3c9851602096c3f4dc8e2149d8a1c29
Valentin <valdiev@live.fr>
2021-01-05 14:23:44 +0100

    header test

--- wp-content/themes/mon_theme/header.php
+++ wp-content/themes/mon_theme/header.php
@@ -5,12 +5,51 @@
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Document</title>
     <link rel="stylesheet" type="text/css" href="<?php echo get_stylesheet_directory_uri(). '/css/header.css' ?>">
+    <link rel="stylesheet" type="text/css" href="<?php echo get_stylesheet_directory_uri(). '/style.css' ?>">
 
     <?php wp_head(); ?>
 </head>
 <body>
+    <nav class="nav">
+    <div class="nav-content">
+        <div class="nav-content--img"></div>
+        <div class="nav-content--nav">
+            <ul>
+                <li><a href="<?php bloginfo('url'); ?>">Accueil</a></li>
+                <li><a href="<?php bloginfo('url'); ?>/regles/">Règles</a></li>
+                <li><a href="<?php bloginfo('url'); ?>/candidats/">Liste des candidats</a></li>
+                <li><a href="<?php bloginfo('url'); ?>/resultats/">Résultats</a></li>
+            </ul>
+        </div>
+    </div>    
+    </nav>
 
-    <h1>test</h1>
-    
+<style>
+
+.nav {
+    width: 100%;
+    background-color: #1637AD;
+}
+.nav .nav-content {
+        display: flex;
+        justify-content: space-between;
+        align-items: center;
+        max-width: 1000px;
+        margin: 0 auto;
+        height: 300px;
+}
+
+.nav .nav-content .nav-content--nav ul {
+    display: flex;
+}
+.nav .nav-content .nav-content--nav ul li {
+    list-style: none;
+}
+.nav .nav-content .nav-content--nav ul li a {
+    text-decoration: none;
+}
+
+</style>
+
 </body>
 </html>
