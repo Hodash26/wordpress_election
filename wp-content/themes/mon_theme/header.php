9ceb9c5cc710fd5d0e8bebe8ce9a7b7537909ecb
Florent <florent.bailly62400@gmail.com>
2021-01-05 11:16:31 +0100

    add styles

--- wp-content/themes/mon_theme/header.php
+++ wp-content/themes/mon_theme/header.php
@@ -4,8 +4,13 @@
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Document</title>
+    <link rel="stylesheet" type="text/css" href="<?php echo get_stylesheet_directory_uri(). '/css/header.css' ?>">
+
+    <?php wp_head(); ?>
 </head>
 <body>
+
+    <h1>test</h1>
     
 </body>
 </html>
--- wp-content/themes/mon_theme/index.php
+++ wp-content/themes/mon_theme/index.php
@@ -6,6 +6,7 @@
     <title>Document</title>
 </head>
 <body>
-    
+    <?php get_header(); ?>
+
 </body>
 </html>
